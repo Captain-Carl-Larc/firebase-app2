ce79e62203dcf7b044ad6e28196947c9d6755503
Captain-Carl-Larc <155651529+Captain-Carl-Larc@users.noreply.github.com>
2024-09-13 22:50:40 +0300

    adding list items

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js"
-import { getDatabase ,ref ,push,onValue } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
+import { getDatabase ,ref ,push,onValue, remove } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
 
 const appSettings = {
     databaseURL: "https://realtime-dtabase-default-rtdb.europe-west1.firebasedatabase.app/"
@@ -20,16 +20,20 @@ const inputEl=document.getElementById("task-input")
 saveBtn.addEventListener("click",saveTask)
 
 onValue(tasksInDb,function(snapshot){
+    clearList()
     let tasksArray=Object.values(snapshot.val())
     for (let i = 0; i < tasksArray.length; i++) {        
-        const taskElements = tasksArray[i];
+        const element = tasksArray[i];
         appendTask(element)
         console.log(element)
     }
     
 })
 function appendTask(task) {
-    taskDisplayEl.innerHTML +=`<li>${task}</li>`
+    let newEl=document.createElement("li")
+    newEl.textContent="task"
+    //taskDisplayEl.innerHTML +=`<li class="bg-green-600 px-4 py-2">${task}</li>`
+    taskDisplayEl.append(newEl)
 
 }
 
@@ -38,4 +42,13 @@ function saveTask() {
     push(tasksInDb,taskValue)    
     inputEl.value=""
     console.log(`${taskValue} added to database.`)
+}
+
+function clearList(){
+    taskDisplayEl.innerHTML=""
+}
+
+function removeTask() {
+    let taskToDeleteID=Object.keys(snap)
+    remove(tasksInDb,taskToDeleteID)
 }
